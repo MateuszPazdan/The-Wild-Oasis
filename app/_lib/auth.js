caaf1e3f629e453cfc5adb5dd81a38ea49bf0c47
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-07-02 00:24:13 +0200

    course finished

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -26,9 +26,9 @@ const authConfig = {
 		async session({ session, user }) {
 			const guest = await getGuest(session.user.email);
 			session.user.guestId = guest.id;
+
 			return session;
 		},
-        
 	},
 	pages: {
 		signIn: '/login',
